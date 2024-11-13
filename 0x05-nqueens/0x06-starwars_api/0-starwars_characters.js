9e8f1d0ba491abf90a61116b0c90eab1f2d8f06c
BIBIRESANMI DAVID <120417611+BIBIREDAVID@users.noreply.github.com>
2024-11-13 18:30:11 +0100

    wew

--- 0x05-nqueens/0x06-starwars_api/0-starwars_characters.js
+++ 0x05-nqueens/0x06-starwars_api/0-starwars_characters.js
@@ -3,51 +3,36 @@
 const request = require('request');
 
 const movieId = process.argv[2];
-const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
-let people = [];
-const names = [];
+const filmEndPoint = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
-const requestCharacters = async () => {
-  await new Promise(resolve => request(filmEndPoint, (err, res, body) => {
+// Function to fetch character names in order
+const getCharacterNames = async () => {
+  // Request the film data
+  request(filmEndPoint, (err, res, body) => {
     if (err || res.statusCode !== 200) {
-      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-    } else {
-      const jsonBody = JSON.parse(body);
-      people = jsonBody.characters;
-      resolve();
+      console.error('Error:', err || `StatusCode: ${res.statusCode}`);
+      return;
     }
-  }));
-};
 
-const requestNames = async () => {
-  if (people.length > 0) {
-    for (const p of people) {
-      await new Promise(resolve => request(p, (err, res, body) => {
+    // Parse the response and get character URLs
+    const jsonBody = JSON.parse(body);
+    const characters = jsonBody.characters;
+
+    // Fetch each character's name in sequence
+    characters.forEach((characterURL) => {
+      request(characterURL, (err, res, body) => {
         if (err || res.statusCode !== 200) {
-          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-        } else {
-          const jsonBody = JSON.parse(body);
-          names.push(jsonBody.name);
-          resolve();
+          console.error('Error:', err || `StatusCode: ${res.statusCode}`);
+          return;
         }
-      }));
-    }
-  } else {
-    console.error('Error: Got no Characters for some reason');
-  }
+
+        // Print each character's name in the order they appear
+        const character = JSON.parse(body);
+        console.log(character.name);
+      });
+    });
+  });
 };
 
-const getCharNames = async () => {
-  await requestCharacters();
-  await requestNames();
-
-  for (const n of names) {
-    if (n === names[names.length - 1]) {
-      process.stdout.write(n);
-    } else {
-      process.stdout.write(n + '\n');
-    }
-  }
-};
-
-getCharNames();
+// Execute the function
+getCharacterNames();
