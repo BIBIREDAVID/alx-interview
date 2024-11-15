d0efab3384ce468cef265ec30710d9f76f5a4c8f
BIBIRESANMI DAVID <120417611+BIBIREDAVID@users.noreply.github.com>
2024-11-15 14:48:29 +0100

    check 0

--- 0x05-nqueens/0x06-starwars_api/0-starwars_characters.js
+++ 0x05-nqueens/0x06-starwars_api/0-starwars_characters.js
@@ -1,38 +1,25 @@
 #!/usr/bin/node
-
 const request = require('request');
+const API_URL = 'https://swapi-api.hbtn.io/api';
 
-const movieId = process.argv[2];
-const filmEndPoint = `https://swapi-api.hbtn.io/api/films/${movieId}`;
-
-// Function to fetch character names in order
-const getCharacterNames = async () => {
-  // Request the film data
-  request(filmEndPoint, (err, res, body) => {
-    if (err || res.statusCode !== 200) {
-      console.error('Error:', err || `StatusCode: ${res.statusCode}`);
-      return;
+if (process.argv.length > 2) {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
     }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
 
-    // Parse the response and get character URLs
-    const jsonBody = JSON.parse(body);
-    const characters = jsonBody.characters;
-
-    // Fetch each character's name in sequence
-    characters.forEach((characterURL) => {
-      request(characterURL, (err, res, body) => {
-        if (err || res.statusCode !== 200) {
-          console.error('Error:', err || `StatusCode: ${res.statusCode}`);
-          return;
-        }
-
-        // Print each character's name in the order they appear
-        const character = JSON.parse(body);
-        console.log(character.name);
-      });
-    });
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
   });
-};
-
-// Execute the function
-getCharacterNames();
+}
